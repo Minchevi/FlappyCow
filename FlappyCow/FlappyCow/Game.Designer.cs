2e1d9e6758d25885185cc31c8085695c4e0c5f4a
Nelly Mincheva <nelly.m.mincheva@gmail.com>
2016-03-10 00:24:16 +0200

    Cow added

---
+++ FlappyCow/FlappyCow/Game.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace FlappyCow
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cow = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.cow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cow
+            // 
+            this.cow.BackColor = System.Drawing.Color.Transparent;
+            this.cow.BackgroundImage = global::FlappyCow.Properties.Resources.cow;
+            this.cow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.cow.Location = new System.Drawing.Point(51, 89);
+            this.cow.Name = "cow";
+            this.cow.Size = new System.Drawing.Size(132, 103);
+            this.cow.TabIndex = 0;
+            this.cow.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.ClientSize = new System.Drawing.Size(779, 428);
+            this.Controls.Add(this.cow);
+            this.KeyPreview = true;
+            this.Name = "Game";
+            this.ShowIcon = false;
+            this.Text = "Flappy Cow";
+            this.Load += new System.EventHandler(this.Game_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cow)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox cow;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ FlappyCow/FlappyCow/Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyCow
+{
+    public partial class Game : Form
+    {
+
+        public int speed = 0;
+        public int gravity = 1;
+
+        public Game()
+        {
+            InitializeComponent();
+        }
+
+        private void Game_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Game_KeyDown(object sender, KeyPressEventArgs e)
+        {
+            speed -= 15;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            speed += gravity;
+            //cow.Location.Y += speed;
+
+        }
+    }
+}
